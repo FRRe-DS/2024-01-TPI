ca2573b86761ff5a2b36da11a44417458d1f7b6c
UniversoParalelo <cmartinrodich@gmail.com>
2024-12-02 16:08:00 -0300

    .

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Deja el host tal como está si es necesario para tu entorno.
+  port: env.int('PORT', process.env.PORT || 1337), // Usa el puerto de la variable de entorno proporcionado por Vercel (o usa 1337 por defecto).
   app: {
     keys: env.array('APP_KEYS'),
   },
